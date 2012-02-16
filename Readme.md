5ee1766123f4ac57ac5ad6d11de70b47a788e170
Ritchie Martori <skawful@gmail.com>
2012-02-15 16:39:36 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,35 @@
+# reval
+
+A JavaScript lib for building continuous execution style programming interfaces.
+
+## Example
+
+Evaluate some javascript.
+
+    function src() {
+    	return 2 + 2 + 2;
+    }
+
+    console.info(reval(src.toString()).run()); // 6
+    
+Print all local variables.
+
+    function locals() {
+      var x = 2
+        , y = 3;
+
+      return x * y;
+    }
+
+    var ctx = reval(locals.toString());
+
+    ctx.watch(function(name, val) {
+      console.info(arguments);
+    });
+
+    ctx.run();
+    
+Output:
+
+    ['x', 2]
+    ['y', 3]
---
+++ test/reval.test.js
@@ -0,0 +1,70 @@
+var reval = require('../')
+  , expect = require('chai').expect;
+
+function example(a, b, c) {
+  var e = 1
+    , f = 2
+    , g = 3;
+
+  return g * f * e;
+}
+
+describe('API', function(){
+  describe('reval(src).run()', function(){
+    it('should execute the given source and return the result', function(done){
+      var result = reval(example.toString()).run();
+      
+      expect(result).to.equal(6);
+      done();
+    })
+  })
+  
+  describe('reval(src).watch(fn)', function(){
+    it('should output the values of the local variables', function(done){
+      var ctx = reval(example.toString());
+      
+      var vals = {}, i = 0;
+      
+      ctx.watch(function(name, val) {
+        vals[name] = val;
+        
+        if(i++ === 2) {
+          expect(vals).to.eql({
+            'e': 1,
+            'f': 2,
+            'g': 3
+          });
+          
+          done();
+        }
+      });
+      
+      ctx.run();
+    })
+  })
+})
+
+function bad() {
+  var i = 99999999;
+  
+  while(i--) {
+    // infinte loop 1
+  }
+  
+  for(var i = 0; i < 0; i++) {
+    // infinite loop 3
+  }
+}
+
+describe('Stress / Security', function(){
+  
+  it('should stop executing after its starts spinning into infinity', function(done){
+    var ctx = reval(bad.toString());
+    
+    ctx.run();
+    
+    done();
+  })
+  
+  
+})
